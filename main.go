2155c61ea4dae7dc082a516b101073ae260038b5
Кирюша <kirillzaporozec48@gmail.com>
2025-03-03 00:09:08 +0700

    загрузка проекта

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type WeatherResponse struct {
+	Main struct {
+		Temp      float64 `json:"temp"`       // Температура
+		FeelsLike float64 `json:"feels_like"` // Ощущается как
+		Pressure  int     `json:"pressure"`   // Атмосферное давление
+		Humidity  int     `json:"humidity"`   // Влажность
+	} `json:"main"`
+	Weather []struct {
+		Description string `json:"description"` // Описание погоды
+	} `json:"weather"`
+	Name string `json:"name"` // Название города
+}
+
+func main() {
+	apiKey := "27f05a07d9424065e5e79058cc75e825" // API-ключ
+
+	var city string
+	fmt.Print("Введите город: ")
+	fmt.Scanln(&city)
+
+	city = url.QueryEscape(city) // подготовка к вставке в url
+
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=ru", city, apiKey)
+
+	response, err := http.Get(url) // выполнение запроса
+	if err != nil {
+		fmt.Println("Ошибка при выполнении запроса:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK { // Проверка статуса ответа
+		fmt.Printf("Ошибка: сервер вернул статус %d\n", response.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(response.Body) // Чтение ответа
+	if err != nil {
+		fmt.Println("Ошибка при чтении ответа:", err)
+		return
+	}
+
+	// fmt.Println("Ответ от API:", string(body))
+
+	var weather WeatherResponse // Разбор JSON-ответа
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		fmt.Println("Ошибка при разборе JSON:", err)
+		return
+	}
+
+	if weather.Name == "" {
+		fmt.Println("Город не найден. Проверьте правильность ввода.")
+		return
+	}
+
+	fmt.Printf("\nПогода в городе %s:\n", weather.Name)
+	fmt.Printf("Температура: %.1f°C\n", weather.Main.Temp)
+	fmt.Printf("Ощущается как: %.1f°C\n", weather.Main.FeelsLike)
+	fmt.Printf("Давление: %d hPa\n", weather.Main.Pressure)
+	fmt.Printf("Влажность: %d%%\n", weather.Main.Humidity)
+	fmt.Printf("Описание: %s\n", weather.Weather[0].Description)
+	bufio.NewReader(os.Stdin).ReadBytes(' ')
+}
